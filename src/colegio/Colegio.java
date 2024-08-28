45080b9d4bc0953784dd5cf6e9ca8c43b6cfd96d
Gabriel Jara <95397129+Gabriel-Jara@users.noreply.github.com>
2024-08-27 21:58:54 -0300

    Formularios agregados

--- src/colegio/Colegio.java
+++ src/colegio/Colegio.java
@@ -10,11 +10,14 @@ public class Colegio {
 
 
     public static void main(String[] args) {
-        HashSet<Materia> materiasDisponibles = new HashSet();
-        HashSet<Alumno> alumnosInscriptos = new HashSet();
-        
+//        HashSet<Materia> materiasDisponibles = new HashSet();
+//        HashSet<Alumno> alumnosInscriptos = new HashSet();
+        FormularioAlumnos alum = new FormularioAlumnos();
+        FormularioInscripcion insc = new FormularioInscripcion();
         FormularioMateria form = new FormularioMateria();
         form.setVisible(true);
+        
+        
     }
 
 }
